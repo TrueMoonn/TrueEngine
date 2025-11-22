72947732961a81bfe01a14b50f3d0496775da39e
JulesMEI <jules.meignal@epitech.eu>
2025-11-22 10:54:44 +0100

    feat: added collision failsafe, reworked coding style

--- src/systems/hitbox.cpp
+++ src/systems/hitbox.cpp
@@ -29,19 +29,27 @@ void hitbox2_sys(ECS::Registry& reg) {
 
     for (auto &&[id, pos, vel, hit, mov]
         : ECS::IndexedZipper(positions, velocities, hitboxs, movable)) {
-        Position2 center(V(pos).x + V(hit).size.x / 2, V(pos).y + V(hit).size.y / 2);
+        Position2 center(V(pos).x + V(hit).size.x / 2,
+            V(pos).y + V(hit).size.y / 2);
 
         for (ECS::Entity cmp : entity_hit(reg, id)) {
             auto other_pos = reg.getComponents<Position2>()[cmp];
             auto other_hit = reg.getComponents<Hitbox>()[cmp];
 
-            if (isBetWeen(center.y, V(other_pos).y, V(other_pos).y + V(other_hit).size.y)) {
+            if (isBetWeen(center.y, V(other_pos).y,
+                V(other_pos).y + V(other_hit).size.y) &&
+                isBetWeen(center.x, V(other_pos).x,
+                V(other_pos).x + V(other_hit).size.x))
+                V(pos).x += 15;
+            if (isBetWeen(center.y, V(other_pos).y,
+                V(other_pos).y + V(other_hit).size.y)) {
                 if (V(other_pos).x < V(pos).x)
                     V(pos).x = V(other_pos).x + V(other_hit).size.x;
                 if (V(other_pos).x > V(pos).x)
                     V(pos).x = V(other_pos).x - V(hit).size.x;
             }
-            if (isBetWeen(center.x, V(other_pos).x, V(other_pos).x + V(other_hit).size.x)) {
+            if (isBetWeen(center.x, V(other_pos).x,
+                V(other_pos).x + V(other_hit).size.x)) {
                 if (V(other_pos).y < V(pos).y)
                     V(pos).y = V(other_pos).y + V(other_hit).size.y;
                 if (V(other_pos).y > V(pos).y)
