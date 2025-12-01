5b8be130a49a516b8ad8dc3e0e847aa15d1ec68d
Ethan GIllaux-Genelle <ethan.gillaux-genelle@epitech.eu>
2025-12-01 14:52:02 +0100

    feat(event.cpp): edit all the event checking with our enum and array

--- src/systems/event.cpp
+++ src/systems/event.cpp
@@ -5,11 +5,16 @@
 ** event.cpp
 */
 
+#include <memory.h>
+#include <variant>
+
 #include <SFML/Window/Event.hpp>
 
 #include <ECS/Zipper.hpp>
 
 #include "systems/event.hpp"
+
+#include "components/event.hpp"
 #include "components/window.hpp"
 #include "components/player.hpp"
 #include "components/position.hpp"
@@ -24,25 +29,23 @@ namespace te {
 
 void playerMovementEvent(ECS::Registry& reg) {
     auto& players = reg.getComponents<Player>();
+    auto& event = reg.getComponents<Event>()[0];
+    auto& velocities = reg.getComponents<Velocity2>();
 
-    for (ECS::Entity e = 0; e < players.size(); ++e) {
-        if (players[e].has_value()) {
-            auto& velocities = reg.getComponents<Velocity2>();
-            if (e < velocities.size() && velocities[e].has_value()) {
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Q))
-                    velocities[e].value().x = P_MOVEMENT.at(P_LEFT_MOV);
-                else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
-                    velocities[e].value().x = P_MOVEMENT.at(P_RIGHT_MOV);
-                else
-                    velocities[e].value().x = 0.0f;
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Z))
-                    velocities[e].value().y = P_MOVEMENT.at(P_TOP_MOV);
-                else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
-                    velocities[e].value().y = P_MOVEMENT.at(P_BOT_MOV);
-                else
-                    velocities[e].value().y = 0.0f;
-            }
-        }
+     for (auto &&[id, pos, vel] : ECS::IndexedZipper(players,
+        velocities)) {
+        if (event.value()._Keys[te::Q])
+             vel.value().x = P_MOVEMENT.at(P_LEFT_MOV);
+        else if (event.value()._Keys[te::D])
+            vel.value().x = P_MOVEMENT.at(P_RIGHT_MOV);
+        else
+            vel.value().x = 0.0f;
+        if (event.value()._Keys[te::Z])
+            vel.value().y = P_MOVEMENT.at(P_TOP_MOV);
+        else if (event.value()._Keys[te::S])
+            vel.value().y = P_MOVEMENT.at(P_BOT_MOV);
+        else
+            vel.value().y = 0.0f;
     }
 }
 
@@ -64,40 +67,101 @@ void create_projectile(ECS::Registry& reg, const Position2 &pos,
 void shootProjectileEvent(ECS::Registry& reg) {
     auto& players = reg.getComponents<Player>();
     auto& position = reg.getComponents<Position2>();
+    auto& event = reg.getComponents<Event>()[0];
 
     for (auto &&[p, pos] : ECS::Zipper(players, position)) {
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))
+        if (event.value()._Keys[te::Up])
             create_projectile(reg, {pos.value().x + 15 - (PROJ_SIZE / 2),
                 pos.value().y + 15 - (PROJ_SIZE / 2) - PROJ_VEL},
                 {0, -PROJ_VEL});
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down))
+        if (event.value()._Keys[te::Down])
             create_projectile(reg, {pos.value().x + 15 - (PROJ_SIZE / 2),
                     pos.value().y + 15 - (PROJ_SIZE / 2) + PROJ_VEL},
                 {0, PROJ_VEL});
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
+        if (event.value()._Keys[te::Left])
             create_projectile(reg, {pos.value().x + 15 - (PROJ_SIZE / 2)
                 - PROJ_VEL, pos.value().y + 15 - (PROJ_SIZE / 2)},
                 {-PROJ_VEL, 0});
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
+        if (event.value()._Keys[te::Right])
             create_projectile(reg, {pos.value().x + 15 - (PROJ_SIZE / 2)
                 + PROJ_VEL, pos.value().y + 15 - (PROJ_SIZE / 2)},
                 {PROJ_VEL, 0});
     }
 }
 
+void manageWindow(ECS::Registry& reg, std::reference_wrapper<Window> win) {
+    auto& event = reg.getComponents<Event>()[0];
+
+    if (event.value()._Keys[te::Escape]) {
+        win.get().close();
+    }
+    if (event.value()._Keys[te::LostFocus]) {
+
+    }
+}
+
+void handleEvent(const sf::Event& event, auto &Event, std::reference_wrapper<Window> win) {
+    if (const auto* e = event.getIf<sf::Event::Closed>()) {
+        win.get().close();
+    }
+    else if (const auto* e = event.getIf<sf::Event::KeyPressed>()) {
+        Event.value()._Keys[(te::Key)e->code] = true;
+        std::cout << (te::Key)e->code << std::endl;
+    }
+    else if (const auto* e = event.getIf<sf::Event::KeyReleased>()) {
+        Event.value()._Keys[(te::Key)e->code] = false;
+    }
+    else if (const auto* e = event.getIf<sf::Event::Resized>()) {
+        sf::View view = win.get().getDefaultView();
+
+        view.setSize({
+            static_cast<float>(e->size.x),
+            static_cast<float>(e->size.y)
+        });
+        win.get().setView(view);
+    }
+    else if (const auto* e = event.getIf<sf::Event::MouseButtonPressed>()) {
+        Event.value()._Systems[te::System::MouseButtonPressed] = true;
+    
+        if (auto it = te::_Mouse.find(e->button); it != te::_Mouse.end()) {
+            size_t idx = it->second;
+            Event.value()._MouseButtons[idx].first = true;
+            Event.value()._MouseButtons[idx].second.x = e->position.x;
+            Event.value()._MouseButtons[idx].second.y = e->position.y;
+        }
+    }
+    else if (const auto* e = event.getIf<sf::Event::MouseMoved>()) {
+        Event.value()._Systems[te::System::MouseMoved] = true;
+    }
+    else if (const auto* e = event.getIf<sf::Event::MouseEntered>()) {
+        Event.value()._Systems[te::System::MouseEntered] = true;
+    }
+    else if (const auto* e = event.getIf<sf::Event::MouseButtonReleased>()) {
+        Event.value()._Systems[te::System::MouseButtonReleased] = true;
+    }
+    else if (const auto *e = event.getIf<sf::Event::FocusLost>()) {
+        Event.value()._Systems[te::System::LostFocus] = true;
+    }
+    else if (const auto *e = event.getIf<sf::Event::FocusGained>()) {
+        Event.value()._Systems[te::System::GainedFocus] = true;
+    }
+}
+
 void manageEvent(ECS::Registry& reg) {
+    auto &Event = reg.getComponents<te::Event>()[0];
+
     std::optional<std::reference_wrapper<Window>> opt_win =
         findActiveWindow(reg.getComponents<Window>());
-    if (!opt_win.has_value())
+    if (!opt_win.has_value() || !Event.has_value() )
         return;
     std::reference_wrapper<Window> win = opt_win.value();
 
-    while (std::optional event = win.get().pollEvent()) {
-        if (event->is<sf::Event::Closed>())
-            win.get().close();
+    while (std::optional<sf::Event> event = win.get().pollEvent()) {
+        handleEvent(*event, Event, win);
     }
     playerMovementEvent(reg);
     shootProjectileEvent(reg);
+    manageWindow(reg, win);
 }
 
 }  // namespace te
