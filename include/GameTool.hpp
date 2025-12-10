8e52340e5d2c5390e87a305d24bb0a9cdb9625c9
PierrePruvost03 <pierre.pruvost@epitech.eu>
2025-12-10 22:51:37 +0100

    fix: getComponent has to return the component get

--- include/GameTool.hpp
+++ include/GameTool.hpp
@@ -14,6 +14,7 @@
 
     #include <ECS/Entity.hpp>
     #include <ECS/Registry.hpp>
+    #include <ECS/SparseArray.hpp>
 
     #include "event/EventManager.hpp"
     #include "config/map_loader.hpp"
@@ -93,8 +94,8 @@ class GameTool {
      * @tparam Component The type of the component to get
      */
     template <typename Component>
-    void getComponent(void) {
-        _reg.getComponents<Component>();
+    ECS::SparseArray<Component>& getComponent(void) {
+        return _reg.getComponents<Component>();
     }
 
     /**
