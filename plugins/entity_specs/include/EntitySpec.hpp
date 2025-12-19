dd995270741b750b0626899dbdfed2d9c299ad1f
JulesDELTEIL <jules.delteil@epitech.eu>
2025-12-19 13:46:45 +0100

    feat(display): update display addon with new signals

--- plugins/entity_specs/include/EntitySpec.hpp
+++ plugins/entity_specs/include/EntitySpec.hpp
@@ -14,9 +14,4 @@
     #include "entity_spec/components/pattern.hpp"
     #include "entity_spec/components/timeout.hpp"
 
-    // #include "entity_spec/systems/deal_damage.hpp"
-    // #include "entity_spec/systems/apply_pattern.hpp"
-    // #include "entity_spec/systems/apply_fragile.hpp"
-    // #include "entity_spec/systems/kill_entity.hpp"
-
     #include "entity_spec/hitbox.hpp"
