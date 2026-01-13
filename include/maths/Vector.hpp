56b910785e7ae53de7d677ba100210cc612c03ff
Ethan GIllaux-Genelle <ethan.gillaux-genelle@epitech.eu>
2026-01-13 16:17:50 +0100

    fix(vector): correct distance calculation

--- include/maths/Vector.hpp
+++ include/maths/Vector.hpp
@@ -90,7 +90,7 @@ struct Vector2 {
     }
 
     int distance(Vector2 Vec) {
-        return (std::sqrt(pow(abs(Vec.y - this->y), 2) + pow(abs(Vec.x - this->y)), 2));
+        return (std::sqrt(pow(abs(this->y - Vec.y), 2) + pow(abs(this->y - Vec.x), 2)));
     }
 
     int length(void) {
