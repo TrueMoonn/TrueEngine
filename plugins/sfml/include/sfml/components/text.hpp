d447caefbab28eca0f03903b24496226a875e9a6
JulesMEI <jules.meignal@epitech.eu>
2026-01-14 12:40:57 +0100

    fix: added text description and real time color and values update

--- plugins/sfml/include/sfml/components/text.hpp
+++ plugins/sfml/include/sfml/components/text.hpp
@@ -23,7 +23,12 @@ namespace sfml {
 /**
  * @brief Text component for the sfml plugin
  *
- * @param fontpath : the
+ * @param font : the path of the font to use
+ * @param str : the text string
+ * @param offset : the offset from the entity's position
+ * @param center : weither the text will be centered on it's position or not
+ * @param size : the character size
+ * @param color : the text's color as a 4 value code.
  */
 struct PLUGIN_API Text : sf::Text {
     Text(const sf::Font &font, const std::string &str
